c22e3478b3b33e33b8dd284933443c6af114ee57
Steven Caudill <scaudill@caudillinc.com>
2018-03-29 18:10:54 +0000

    adding basic Apache cookbook

---
+++ cookbooks/stevecookbook/README.md
@@ -0,0 +1,4 @@
+# stevecookbook
+
+TODO: Enter the cookbook description here.
+
---
+++ cookbooks/stevecookbook/recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: stevecookbook
+# Recipe:: default
+#
+# Copyright:: 2018, The Authors, All Rights Reserved.
